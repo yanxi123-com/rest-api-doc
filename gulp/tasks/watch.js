a0a060c6178ecfba31f547b063bcb298d007b3d1
blogbbs <blogbbs@gmail.com>
2015-09-11 08:43:54 +0800

    bugfix

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var config        = require('../config');
-var gulp          = require('gulp');
+var config = require('../config');
+var gulp = require('gulp');
 
-gulp.task('watch', ['browserSync', 'server'], function() {
+gulp.task('watch', ['browserSync', 'server'], function () {
 
-  // Scripts are automatically watched and rebundled by Watchify inside Browserify task
-  gulp.watch(config.scripts.src, ['lint']);
-  gulp.watch(config.styles.src,  ['styles']);
-  gulp.watch(config.views.watch, ['views']);
+    // Scripts are automatically watched and rebundled by Watchify inside Browserify task
+    gulp.watch(config.scripts.src, ['lint']);
+    gulp.watch(config.styles.src, ['styles']);
+    gulp.watch(config.views.watch, ['views']);
 
 });
